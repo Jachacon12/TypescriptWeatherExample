5cdf2a29cc5128c06a3e1fe18c3f4e91873a801c
jorgechac <55976500+jorgechac@users.noreply.github.com>
2024-06-10 20:50:38 +0200

    feat: Using TypeScript type assertions

--- src/networking/weather.ts
+++ src/networking/weather.ts
@@ -1,22 +1,27 @@
 import { WeatherResponse } from '../model/weatherResponse';
-import { response as fallbackData } from '../mock/response';
+import { response as data } from '../mock/response';
 
-// TODO: Create an async function with an argument called city to return the that of the endpoint
+const fallbackData: WeatherResponse = JSON.parse(data);
 
-export const fetchData = async (city: string) => {
+export const fetchData = async (city: string): Promise<WeatherResponse> => {
   try {
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ac6f213887b95d0b8171b342e702e112&units=metric`
     );
 
     if (!response.ok) {
-      return JSON.parse(fallbackData);
+      // If response not ok, return fallback data
+      return fallbackData;
     }
 
-    return await response.json();
+    const jsonData = await response.json();
+    // Using type assertion to ensure jsonData matches WeatherResponse
+    const weatherResponse = jsonData as WeatherResponse;
+
+    return weatherResponse;
   } catch (error) {
     console.error('Error:', error);
-    // Return fallback data in case of an error
-    return JSON.parse(fallbackData);
+    // Consolidating error handling
+    return fallbackData;
   }
 };
