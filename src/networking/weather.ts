512561a9f30fd0b6a653bf0281e1b40d1af7cf31
jorgechac <55976500+jorgechac@users.noreply.github.com>
2024-06-10 15:41:40 +0200

    feat: network function

--- src/networking/weather.ts
+++ src/networking/weather.ts
@@ -1,7 +1,17 @@
-import { WeatherResponse } from "../model/weatherResponse";
-
-const city = 'Example';
-
-const API_CURRENT = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ac6f213887b95d0b8171b342e702e112&units=metric`;
+import { WeatherResponse } from '../model/weatherResponse';
+import { response as fallbackData } from '../mock/response';
 
 // TODO: Create an async function with an argument called city to return the that of the endpoint
+
+export const fetchData = async (city: string) => {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ac6f213887b95d0b8171b342e702e112&units=metric`
+  );
+
+  if (!response.ok) {
+    return JSON.parse(fallbackData);
+  }
+  if (response.ok) {
+    return await response.json();
+  }
+};
