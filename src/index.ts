135270d7b2c611fe86e8183bc919a8fdc955529c
jorgechac <55976500+jorgechac@users.noreply.github.com>
2024-06-10 15:42:21 +0200

    feat: function to fetch new data based on input value adn update interface

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,34 @@ import './styles/main.scss';
 
 import { response } from './mock/response';
 // Import the API request method
+import { fetchData } from './networking/weather';
 import {
   buttonClick,
   getCity,
   updateInteface,
 } from './dom-manipulation/domManipulation';
 
-// Add an event listener to the button
+const defaultCity = 'London';
 
-// Create an async function to call the API method
-
-// Add event listener to button
-buttonClick?.addEventListener('click', async => {
-  getCity();
-});
+// Initialize app
 updateInteface(JSON.parse(response));
+
+// Add an event listener to the button
+buttonClick?.addEventListener('click', async () => {
+  // Get new value of location input, fallback to defaultCity if undefined
+  const city = getCity();
+  const newCity = city.length > 0 && city !== '-' ? city : defaultCity;
+
+  // Create an async function to call the API method
+  // fetch new data base on the value
+  const newResponse = await fetchData(newCity)
+    .then(data => {
+      return data;
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+
+  // update interface based response
+  if (newResponse) updateInteface(newResponse);
+});
